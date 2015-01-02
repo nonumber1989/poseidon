2d8d0befe018ecf16f1366015f66258f14863570
nonumber1989@gmail.com <nonumber7>
2015-01-02 15:25:49 +0800

    mock zhihu

--- app/scripts/controllers/subject/subjectController.js
+++ app/scripts/controllers/subject/subjectController.js
@@ -4,7 +4,7 @@ var asgard = angular.module('asgard');
 
 asgard.controller('SubjectController', ['$scope','$resource',function ($scope,$resource) {
 
-    $scope.subjectResource = $resource('api/subject/subjects.json');
+    $scope.subjectResource = $resource('api/subject/theSubjects.json');
     $scope.subjectResource.query(function(data){
         $scope.subjects = data;
     });
