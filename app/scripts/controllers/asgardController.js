40cfa4735e167286003835725d36fd9a7b87f9f5
nonumber1989@gmail.com <nonumber7>
2015-01-02 13:35:40 +0800

    mock zhihu

--- app/scripts/controllers/asgardController.js
+++ app/scripts/controllers/asgardController.js
@@ -1,15 +1,41 @@
-/**
- * Created by seven on 2014/10/28.
- */
+'use strict';
+
 var asgard = angular.module('asgard');
-asgard.controller('AsgardController', ['$scope','$resource',function ($scope,$resource) {
+asgard.controller('AsgardController', ['$scope','$resource','$modal', '$log',function ($scope,$resource,$modal, $log) {
     $scope.accountResource = $resource('api/people/account.json');
     $scope.accountResource.get(function(data){
         $scope.currentAccount = data;
     });
 
-    $scope.subjectsResource = $resource('api/subject/subjects.json');
-    $scope.subjectsResource.query(function(data){
+    $scope.subjectResource = $resource('api/subject/subjects.json');
+    $scope.subjectResource.query(function(data){
         $scope.subjects = data;
     });
+
+    $scope.collectionResource = $resource('api/collection/collections.json');
+    $scope.collectionResource.query(function(data){
+        $scope.collections = data;
+    });
+
+
+    $scope.open = function (size) {
+        var modalInstance = $modal.open({
+            templateUrl: 'collectionDialog.html',
+            controller: 'CollectionDialogController',
+            size: size,
+            resolve: {
+                collections: function () {
+                    return $scope.collections;
+                }
+            }
+        });
+        modalInstance.result.then(function (selectedItem) {
+            $scope.selected = selectedItem;
+        }, function () {
+            $log.info('Modal dismissed at: ' + new Date());
+        });
+    };
+
 }]);
+
+
