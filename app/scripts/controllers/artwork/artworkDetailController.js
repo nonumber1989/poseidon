51134b0eb2e5ffd7168d1c10810ef5bcd68f76a0
nonumber1989@gmail.com <nonumber7>
2015-01-04 22:55:00 +0800

    mock zhihu

--- app/scripts/controllers/artwork/artworkDetailController.js
+++ app/scripts/controllers/artwork/artworkDetailController.js
@@ -5,7 +5,12 @@ var asgard = angular.module('asgard');
 asgard.controller('ArtworkDetailController', ['$scope','$resource',function ($scope,$resource) {
 
     $scope.artworkDetailResource = $resource('api/artwork/artwork.json');
-    $scope.artworkDetailResource.get(function(data){
+    $scope.artworkDetailResource.query(function(data){
         $scope.theArtwork = data;
+    });
+
+    $scope.tabsetResource = $resource('api/artwork/artworkDetail.json');
+    $scope.tabsetResource.query(function(data){
+        $scope.tabs = data;
     });
 }]);
