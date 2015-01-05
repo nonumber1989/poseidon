2148d2cd9adb8b36a7cc40911642419f233ffba9
nonumber1989@gmail.com <nonumber7>
2015-01-05 23:47:53 +0800

    make some changes

--- app/scripts/controllers/setting/settingController.js
+++ app/scripts/controllers/setting/settingController.js
@@ -3,10 +3,15 @@
 var asgard = angular.module('asgard');
 
 asgard.controller('SettingController', ['$scope','$resource','$stateParams','$window',function ($scope,$resource,$stateParams,$window) {
-    console.log($stateParams.status+"---nonumber1989----");
+
     $scope.tabsetResource = $resource('api/setting/tabset.json');
     $scope.tabsetResource.query(function(data){
         $scope.tabs = data;
+        angular.forEach($scope.tabs, function(tab){
+            if(tab.field == $stateParams.status){
+                tab.active = true;
+            }
+        });
     });
     $scope.alertMe = function() {
         setTimeout(function() {
