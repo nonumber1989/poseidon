55d20ccd5225529258a0d22af2ed426cf65b319c
nonumber1989 <nonumber1989@gmail.com>
2015-02-04 10:14:38 +0800

    change sevenup-server

--- sevenup-server.js
+++ sevenup-server.js
@@ -1,28 +1,41 @@
 'use strict';
-var util = require('util'),
-    colors = require('colors'),
-    http = require('http'),
-    httpProxy = require('http-proxy');
 
+var express = require('express'),
+    http = require('http'),
+    httpProxy = require('http-proxy'),
+    colors = require('colors'),
+    slash   = require('express-slash'),
+    path = require('path'),
+    util = require('util'),
+    url = require('url');
+
+var proxy = httpProxy.createProxyServer({});
+var staticPath = __dirname + '/app';
 //
 // Setup proxy server with forwarding
 //
-httpProxy.createServer({
-    forward: {
-        port: 9019,
-        host: 'localhost'
-    }
-}).listen(8080);
-
+httpProxy.createProxyServer({});
 //
 // Target Http Forwarding Server
 //
 http.createServer(function (req, res) {
     util.puts('Receiving forward for: ' + req.url);
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write('request successfully forwarded to: ' + req.url + '\n' + JSON.stringify(req.headers, true, 2));
-    res.end();
+    proxy.web(req, res, { target: 'http://localhost:10000/warship' });
+//    res.writeHead(200, { 'Content-Type': 'text/plain' });
+//    res.write('request successfully forwarded to: ' + req.url + '\n' + JSON.stringify(req.headers, true, 2));
+//    res.end();
 }).listen(9019);
 
 util.puts('http proxy server '.blue + 'started '.green.bold + 'on port '.blue + '8080 '.yellow + 'with forward proxy'.magenta.underline);
 util.puts('http forward server '.blue + 'started '.green.bold + 'on port '.blue + '9019 '.yellow);
+
+/***** start a server use static *****/
+var app = express();
+app.enable('strict routing');
+app.use(slash());
+app.get('/warship/', function(req, res){
+        res.sendfile(staticPath+'/index.html');
+});
+app.use('/warship/',express.static(staticPath));
+var server = http.createServer(app);
+server.listen(10000);
