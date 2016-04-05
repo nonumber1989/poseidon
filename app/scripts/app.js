958c9d1343f339092101e556d64a13d199489e29
nonumber1989 <nonumber1989@gmail.com>
2016-04-05 22:12:23 +0800

    ok i revert my thinking

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -26,28 +26,111 @@ var asgard =
     'angularFileUpload',
     'pascalprecht.translate'
   ])
-asgard.config(['$stateProvider', '$urlRouterProvider','$resourceProvider','$translateProvider','navigationProvider','i18nProvider',function($stateProvider, $urlRouterProvider,$resourceProvider,$translateProvider,navigationProvider,i18nProvider){
+asgard.config(['$stateProvider', '$urlRouterProvider', '$resourceProvider', '$translateProvider', 'i18nProvider', function($stateProvider, $urlRouterProvider, $resourceProvider, $translateProvider, i18nProvider) {
   $translateProvider.useStaticFilesLoader({
     prefix: '/api/languages/',
     suffix: '.json'
   });
-  navigationProvider.setNavigationUrl('api/navigation.json');
-  //var i18nUrls = ['api/i18n/en.json','api/i18n/zh.json'];
-  //i18nProvider.setI18nUrlArray(i18nUrls);
+
   $translateProvider.preferredLanguage('zh');
+
   $urlRouterProvider.otherwise("/home");
-    $stateProvider
-      .state('home', {
-        url: "/home",
-        templateUrl: "views/layout/home.html",
-        controller:"LayoutController"
-      });
+  $stateProvider
+    .state('home', {
+      url: "/home",
+      templateUrl: "views/layout/home.html",
+      controller: "LayoutController"
+    })
+    .state('about', {
+      "url": "/about",
+      "templateUrl": "../views/common/about.html",
+      "controller": "AboutController"
+    })
+    .state('contact', {
+      "url": "/contact",
+      "templateUrl": "../views/common/contact.html",
+      "controller": "ContactController"
+    })
+    .state('account', {
+      "url": "/account/:id",
+      "templateUrl": "views/account/account.html",
+      "controller": "AccountController"
+    })
+    .state('setting', {
+      "url": "/setting/:status",
+      "templateUrl": "views/setting/setting.html",
+      "controller": "SettingController"
+    })
+    .state('inbox', {
+      "url": "/inbox",
+      "templateUrl": "views/inbox/inbox.html",
+      "controller": "InboxController"
+    })
+    .state('artwork', {
+      "url": "/artwork",
+      "templateUrl": "../views/artwork/artwork.html",
+      "controller": "ArtworkController"
+    })
+    .state('artwork.detail', {
+      "url": "/:id",
+      "templateUrl": "../views/artwork/artworkDetail.html",
+      "controller": "ArtworkDetailController"
+    })
+    .state('location', {
+      "url": "/location",
+      "templateUrl": "../views/location/location.html",
+      "controller": "LocationController"
+    })
+    .state('location.event', {
+      "url": "/event",
+      "templateUrl": "../views/location/event/event.html",
+      "controller": "EventController"
+    })
+    .state('location.event.detail', {
+      "url": "/:id",
+      "templateUrl": "../views/location/event/eventDetail.html",
+      "controller": "EventDetailController"
+    })
+
+
+  .state('location.host', {
+      "url": "/host",
+      "templateUrl": "../views/location/host/host.html",
+      "controller": "HostController"
+    })
+    .state('location.host.explore', {
+      "url": "/explore",
+      "templateUrl": "../views/location/host/hostExplore.html",
+      "controller": "HostExploreController"
+    })
+    .state('location.host.detail', {
+      "url": "/:id",
+      "templateUrl": "../views/location/host/hostDetail.html",
+      "controller": "HostDetailController"
+    })
+    .state('asgard', {
+      "url": "/asgard",
+      "templateUrl": "views/asgard.html",
+      "controller": "AsgardController"
+    })
+    .state('asgard.topic', {
+      "url": "/topic",
+      "templateUrl": "views/topic/topic.html",
+      "controller": "TopicController"
+    })
+    .state('asgard.topic.detail', {
+      "url": "/:id",
+      "templateUrl": "views/topic/topicDetail.html",
+      "controller": "TopicDetailController"
+    })
+    .state('asgard.topic.follower', {
+      "url": "/:id/follower",
+      "templateUrl": "views/topic/topicFollowers.html",
+      "controller": "TopicFollowerController"
+    });
 }]);
-asgard.run([ '$rootScope', '$state', '$stateParams','navigation','i18n',function ($rootScope,  $state,   $stateParams,navigation,i18n) {
-	  navigation.setUpNavigation();
-      //i18n.setUpI18nByArray();
-     $rootScope.$state = $state;
-     $rootScope.$stateParams = $stateParams;
-    }
-  ]
-);
+asgard.run(['$rootScope', '$state', '$stateParams', 'i18n', function($rootScope, $state, $stateParams, i18n) {
+  //i18n.setUpI18nByArray();
+  $rootScope.$state = $state;
+  $rootScope.$stateParams = $stateParams;
+}]);
